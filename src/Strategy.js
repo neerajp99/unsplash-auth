e27463bbba86597e1928f3f667e5557a55d3473e
neerajp99 <neerajp1999@gmail.com>
2020-07-18 10:28:22 +0530

    Add setup

--- src/Strategy.js
+++ src/Strategy.js
@@ -8,12 +8,13 @@ const jwt = require('jsonwebtoken')
 const crypto = require('crypto')
 const util = require('util')
 const querystring = require('querystring')
+const InternalOAuthError = require('passport-oauth2').InternalOAuthError
 
 /**
  * `Strategy` constructor.
  *
  * The Unsplash authentication strategy authenticates requests by delegating to
- * Unsplash using the OAuth protocol.
+ * Unsplash using the OAuth protocol.d
  *
  * Applications must supply a `verify` callback which accepts a `token`,
  * `tokenSecret` and service-specific `profile`, and then calls the `done`
@@ -21,17 +22,33 @@ const querystring = require('querystring')
  * credentials are not valid.  If an exception occured, `err` should be set.
  *
  * Options:
- *   - `clientId`     	identifies client to Unsplash
- *   - `clientSecret`   secret used to establish ownership of the consumer key
- *   - `callbackURL`    URL to which Unsplash will redirect the user after obtaining authorization
- *   - `scope`          Array of permission scopes to request
- *                      Check the official documentation for valid scopes to pass as an array.
+ *   - `clientId`     		identifies client to Unsplash
+ *   - `clientSecret`   	secret used to establish ownership of the consumer key
+ *   - `callbackURL`    	URL to which Unsplash will redirect the user after obtaining authorization
+ *   - `passReqToCallback`  when `true`, `req` is the first argument to the verify callback (default: `false`)
+ *   - `scope`          	Array of permission scopes to request
+ *                      	Check the official documentation for valid scopes to pass as an array.
  *
- * @param {Object} options
- * @param {Function} verify
- * @api public
+ * @constructor
+ * @param {StrategyOptions} options
+ * @param {function} verify
+ * @access public
  */
- function Strategy(){}
+function Strategy(options, verify) {
+    if (typeof options == 'function') {
+        verify = options;
+        options = undefined;
+    }
+    options = options || {};
+    options.authorizationURL = options.authorizationURL || 'https://unsplash.com/oauth/authorize';
+    options.tokenURL = options.tokenURL || 'https://unsplash.com/oauth/token';
+    options.passReqToCallback = options.passReqToCallback === undefined ? true : options.passReqToCallback;
+    // Make the OAuth call here
+    OAuth2Strategy.call(this, options, verify);
+    this.name = 'unsplash';
+}
 
-
-
+/**
+ * Inherit from `OAuth2Strategy`.
+ */
+util.inherits(Strategy, OAuth2Strategy);
