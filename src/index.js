c4130b1b20374c0054e114750c0159eebffcaf72
neerajp99 <neerajp1999@gmail.com>
2020-07-18 10:28:10 +0530

    Add root

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/**
+ * Module dependencies.
+ */
+let Strategy = require('./Strategy');
+
+/**
+ * Expose `Strategy` directly from package.
+ */
+exports = module.exports = Strategy;
+
+
+/**
+ * Expose constructors.
+ */
+exports.Strategy = Strategy;
+
